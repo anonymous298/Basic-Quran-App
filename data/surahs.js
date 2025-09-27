66d876479ed3306eb9858554ba05f734d1d172fb
anonymous298 <tackletalha@gmail.com>
2025-09-27 21:30:44 +0500

    surah data created

---
+++ data/surahs.js
@@ -0,0 +1,74 @@
+
+
+export const surahs = [
+  {
+    id: 1,
+    title: "Surat AL-Baqarah",
+    reader: "Abdullah Awad al-Juhani",
+    src: "/surahs/al_baqarah.mp3",
+    image: "/images/quranimage.webp",
+  },
+  {
+    id: 2,
+    title: "Surat AL-Fatihah",
+    reader: "Abdullah Awad al-Juhani",
+    src: "/surahs/al_fatiha.mp3",
+    image: "/images/quranimage.webp",
+  },
+  {
+    id: 3,
+    title: "Surat An-Nisa",
+    reader: "Abdullah Awad al-Juhani",
+    src: "/surahs/an_nisa.mp3",
+    image: "/images/quranimage.webp",
+  },
+  {
+    id: 4,
+    title: "Surat Ali \'Imran",
+    reader: "Abdullah Awad al-Juhani",
+    src: "/surahs/al_imran.mp3",
+    image: "/images/quranimage.webp",
+  },
+  {
+    id: 5,
+    title: "Surat Al-Ma\'idah",
+    reader: "Abdullah Awad al-Juhani",
+    src: "/surahs/al_maidah.mp3",
+    image: "/images/quranimage.webp",
+  },
+  {
+    id: 6,
+    title: "Surat Al-An\'am",
+    reader: "Abdullah Awad al-Juhani",
+    src: "/surahs/al_anam.mp3",
+    image: "/images/quranimage.webp",
+  },
+  {
+    id: 7,
+    title: "Surat Al-A\'raf",
+    reader: "Abdullah Awad al-Juhani",
+    src: "/surahs/al_araf.mp3",
+    image: "/images/quranimage.webp",
+  },
+  {
+    id: 8,
+    title: "Surat Al-Anfal",
+    reader: "Abdullah Awad al-Juhani",
+    src: "/surahs/al_anfal.mp3",
+    image: "/images/quranimage.webp",
+  },
+  {
+    id: 9,
+    title: "Surat At-Tawbah",
+    reader: "Abdullah Awad al-Juhani",
+    src: "/surahs/at_tawbah.mp3",
+    image: "/images/quranimage.webp",
+  },
+  {
+    id: 10,
+    title: "Surat Yunus",
+    reader: "Abdullah Awad al-Juhani",
+    src: "/surahs/surah_yunus.mp3",
+    image: "/images/quranimage.webp",
+  },
+]
