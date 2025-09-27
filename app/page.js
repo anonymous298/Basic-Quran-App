50b3d7c6942f7ef6ad364ad3040f78c3d492a623
anonymous298 <tackletalha@gmail.com>
2025-09-27 21:32:39 +0500

    page.js updated

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import MainContent from "@/components/MainContent";
+// import PlayerComponent from "@/components/PlayerComponent";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-full overflow-auto">
+      <MainContent />
+    </div>
+  );
+}
